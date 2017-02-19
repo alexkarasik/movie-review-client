c3d903cb3ecd84f2cf32972ce94c68f035a1842d
kinkorea <alexkarasik89@gmail.com>
2017-02-19 13:04:14 -0500

    Added in an api method to have the user be able to look at the reviews for each movie.

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -18,13 +18,10 @@ const signIn = function (data) {
   });
 };
 
-//changePassword requires authentication, which was a concept i struggled with quite a bit. Our url will get us to the user id, which can be retrived from store after a user has successfully signed in, then uses the patch method to edit the token.
 const changePassword = function (data) {
   return $.ajax({
     url: `${config.apiOrigin}/change-password/${store.user.id}`,
-  // we are actually making an edit, so we want to use the patch method here
     method: 'PATCH',
-//headers allows us to perform actions on the http request. Just like the url needed the id to know which user we are changing the password for, we will need that users special and unique token for the authorization to change their password.
     headers: {
       Authorization: `Token token=${store.user.token}`,
     },
@@ -43,6 +40,16 @@ const signOut = function () {
   });
 };
 
+const searchReviews = function() {
+  return $.ajax({
+    url: config.apiOrigin + '/reviews',
+    method: 'GET',
+    headers: {
+    Authorization: `Token token=${store.user.token}`
+  },
+  });
+};
+
 const showGames = function () {
   return $.ajax({
     url: config.apiOrigin + '/games',
@@ -53,44 +60,44 @@ const showGames = function () {
   });
 };
 
-const createGames = function (data) {
-  return $.ajax({
-    url: config.apiOrigin + '/games',
-    method: 'POST',
-    headers: {
-    Authorization: `Token token=${store.user.token}`
-  },
-  data
-  });
-};
-
-//This was the API function I struggled with the most. We are getting into the token of the id of the user and changing the current player, index and status on each turn. I don't want to overthink this too much, but we had to think of what paramters are being updated. The currentPlayer, status of the game and where the move is going are the things that get changed. The index and currentplayer are pieces of information contained with the individual cells that make up the game, which is information stored in the data.
-const updateGame = function(index, currentPlayer, over) {
-  return $.ajax({
-    url: config.apiOrigin + '/games/' + store.game.id,
-    method: 'PATCH',
-    headers: {
-      Authorization: `Token token=${store.user.token}`
-    },
-    data: {
-      game: {
-        cell: {
-          index: index,
-          value: currentPlayer,
-        },
-        over: over,
-      }
-    },
-  });
-};
+// const createGames = function (data) {
+//   return $.ajax({
+//     url: config.apiOrigin + '/games',
+//     method: 'POST',
+//     headers: {
+//     Authorization: `Token token=${store.user.token}`
+//   },
+//   data
+//   });
+// };
+//
+// const updateGame = function(index, currentPlayer, over) {
+//   return $.ajax({
+//     url: config.apiOrigin + '/games/' + store.game.id,
+//     method: 'PATCH',
+//     headers: {
+//       Authorization: `Token token=${store.user.token}`
+//     },
+//     data: {
+//       game: {
+//         cell: {
+//           index: index,
+//           value: currentPlayer,
+//         },
+//         over: over,
+//       }
+//     },
+//   });
+// };
 module.exports = {
   signUp,
   signIn,
   changePassword,
   signOut,
-  createGames,
+  searchReviews,
+  //createGames,
   showGames,
-  updateGame,
+  //updateGame,
 };
 
 
